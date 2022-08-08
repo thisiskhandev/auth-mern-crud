028be25f24cd72ee0cd768709c0fb4c13ea8fcc6
thisiskhandev <hassankhan427726@gmail.com>
2022-08-08 17:57:05 +0500

    Individual data is getting by email

--- server/router/auth.js
+++ server/router/auth.js
@@ -42,14 +42,24 @@ router.get("/get", async (req, res) => {
   }
 });
 
-router.get("/", (req, res) => {
-  console.log(`Home called from routerJS!`);
-  res.send(`Hello from server - RouterJS`);
+router.get("/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userLogin = await User.findOne({ email: email });
+    console.log(userLogin);
+    if (!userLogin) {
+      res.status(400).json({ message: "Please enter correctly data!" });
+    } else {
+      res.status(201).json({ message: userLogin });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.get("/", (req, res) => {
-  console.log(`Home called!`);
-  res.send(`Hello from server - Homepage`);
+  console.log(`Home called from routerJS!`);
+  res.send(`Hello from server - RouterJS`);
 });
 
 router.get("/about", middleware, (req, res) => {
